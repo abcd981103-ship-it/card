f39071d5e09a2c7f3e91da598a53c1976f69d911
abcd981103-ship-it <abcd981103@gmail.com>
2026-08-19 14:49:54 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'card-tongjang-shell-v1';
+const SHELL_FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES).catch(() => {}))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// API(구글 Apps Script) 요청은 캐시하지 않고 그대로 통과시킴 (오프라인 폴백은 앱 자체 localStorage 로직이 담당)
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = event.request.url;
+  if (url.indexOf('script.google.com') >= 0 || url.indexOf('script.googleusercontent.com') >= 0) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request).then((res) => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
